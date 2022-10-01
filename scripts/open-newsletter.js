475638d66009276a30c66b436fa1d79603da3c18
Traptricker <85963782+Traptricker@users.noreply.github.com>
2022-10-01 17:26:12 -0400

    Finished newsletter page

--- scripts/open-newsletter.js
+++ scripts/open-newsletter.js
@@ -1,7 +1,7 @@
 let articles = document.getElementsByClassName("article-container");
 let newsletter = document.getElementById("page-section");
 
-function hideNewsletters() {
+function showNewsletters() {
   newsletter.style.display = "block";
   for (let article of articles) {
     article.style.display = "none";
@@ -12,4 +12,5 @@ function showArticle(number) {
     newsletter.style.display = "none";
     let article = document.getElementById(number.toString());
     article.style.display = "block";
+    window.scrollBy(0, -10000);
 }
